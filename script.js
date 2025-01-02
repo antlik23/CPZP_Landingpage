1426b383cd8d19f92056cc6a368e913ca833fa5f
Jan Antl <antlik@dontspam.me>
2025-01-02 02:06:45 +0100

    Init

---
+++ script.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+// JavaScript pro záložky
+const tabList = document.querySelector('.tab-list');
+const tabContent = document.querySelector('.tab-content');
+
+tabList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        const activeTab = document.querySelector('.tab-list li.active');
+        activeTab.classList.remove('active');
+        event.target.classList.add('active');
+
+        const activeContent = document.querySelector('.tab-content > div.active');
+        activeContent.classList.remove('active');
+        document.getElementById(event.target.dataset.tab).classList.add('active');
+    }
+});
