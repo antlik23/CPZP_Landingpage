1bd5d420d77d64c7e96c682b8a6cc4328ee9494b
Jan Antl <antlik@dontspam.me>
2025-01-06 08:34:45 +0100

    Add autoplay for swiper

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const swiper = new Swiper('.mySwiper', {
         el: '.swiper-pagination',
         clickable: true,
     },
+    autoplay: {
+        delay: 5000,
+    }
 });
 
 // JavaScript pro záložky
